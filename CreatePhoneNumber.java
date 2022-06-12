dc4a1b3d9a2b0f0abe3eb6beb893a9de52154299
SaintDK <ivan.yakimishin.00@mail.ru>
2022-06-12 19:07:00 -0400

    commit

--- CreatePhoneNumber.java
+++ CreatePhoneNumber.java
@@ -2,16 +2,17 @@ public class Kata {
     public static String createPhoneNumber(int[] numbers) {
         String result = "(";
 
-        for(int numbers_i = 0; numbers_i < numbers.length; numbers_i++){
-            result += numbers[numbers_i];
+        for(int numbers_x = 0; numbers_x < numbers.length; numbers_x++){
+            result += numbers[numbers_x];
 
-            if(numbers_i == 2){
+            if(numbers_x == 2){
                 result += ") ";
-            } else if (numbers_i == 5){
+            } else if (numbers_x == 5){
                 result += "-";
             }
         }
 
         return result;
+
     }
 }
