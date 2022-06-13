080a670fec79b6f9dc4744b5e3934e0ddb43bd37
SaintDK <ivan.yakimishin.00@mail.ru>
2022-06-13 17:02:55 -0400

    commit

---
+++ kyu_8/Convert_boolean_values_to_strings_Yes_or_No/YesOrNo.java
@@ -0,0 +1,14 @@
+package kyu_8.Convert_boolean_values_to_strings_Yes_or_No;
+
+class YesOrNo
+{
+    public static String boolToWord(boolean b)
+    {
+        if (b == true){
+            return "Yes";
+        }else{
+            return "No";
+        }
+    }
+
+}
