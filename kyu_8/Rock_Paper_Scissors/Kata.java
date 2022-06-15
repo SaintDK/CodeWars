a7c42b4bbf744ba7becbef17d7d314a3a1de3577
SaintDK <ivan.yakimishin.00@mail.ru>
2022-06-15 15:51:21 -0400

    commit

---
+++ kyu_8/Rock_Paper_Scissors/Kata.java
@@ -0,0 +1,13 @@
+package kyu_8.Rock_Paper_Scissors;
+
+public class Kata {
+    public static String rps(String p1, String p2) {
+        if (p1.equals("rock") & p2.equals("scissors") || p1.equals("scissors") & p2.equals("paper") || p1.equals("paper") & p2.equals("rock")){
+            return "Player 1 won!";
+        }
+        if (p2.equals("rock") & p1.equals("scissors") || p2.equals("scissors") & p1.equals("paper") || p2.equals("paper") & p1.equals("rock")){
+            return "Player 2 won!";
+        }
+        return "Draw!";
+    }
+}
