d3e8a467d9978f891b3ea4c330b31e2a89120bc3
SaintDK <ivan.yakimishin.00@mail.ru>
2022-06-28 14:20:46 -0400

    commit

---
+++ kyu_8/Function_3_multiplying_two_numbers/Kata.java
@@ -0,0 +1,8 @@
+package kyu_8.Function_3_multiplying_two_numbers;
+
+class Kata {
+
+    public static int multiply(int v1, int v2){
+        return v1 * v2;
+    }
+}
---
+++ kyu_8/Function_3_multiplying_two_numbers/SolutionTest.java
@@ -0,0 +1,16 @@
+package kyu_8.Function_3_multiplying_two_numbers;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    Kata K = new Kata();
+    @Test
+    public void sampleTest() {
+        assertEquals(4, K.multiply(2, 2));
+        assertEquals(10, K.multiply(5, 2));
+        assertEquals(100, K.multiply(100, 1));
+        assertEquals(0, K.multiply(0, 1000));
+    }
+}
