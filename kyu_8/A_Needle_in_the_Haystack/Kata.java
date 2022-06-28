1e3290eb5e24b176a451c03b039dda1cb6f9637d
SaintDK <ivan.yakimishin.00@mail.ru>
2022-06-28 07:05:56 -0400

    commit

--- kyu_8/A_Needle_in_the_Haystack/Kata.java
+++ kyu_8/A_Needle_in_the_Haystack/Kata.java
@@ -2,7 +2,6 @@ package kyu_8.A_Needle_in_the_Haystack;
 
 public class Kata {
     public static String findNeedle(Object[] haystack) {
-        int sum = 0;
         for (int i = 0; i < haystack.length; i++) {
             if (haystack[i] == "needle") {
                 return "found the needle at position "+ i;
