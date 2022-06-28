870836d4f90f86191c5b6496d7c6d94206524d0c
SaintDK <ivan.yakimishin.00@mail.ru>
2022-06-28 06:39:08 -0400

    commit

---
+++ kyu_8/How_good_are_you_really/Kata.java
@@ -0,0 +1,16 @@
+package kyu_8.How_good_are_you_really;
+
+public class Kata {
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        int sum = 0;
+        for (int i = 0; i < classPoints.length; i++) {
+            sum += classPoints[i];
+        }
+        sum = sum / classPoints.length;
+        if (yourPoints < sum) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
