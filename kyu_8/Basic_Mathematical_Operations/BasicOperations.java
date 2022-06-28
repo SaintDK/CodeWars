898b9996816793c6895f8a3e8697eee5d9fbb9a8
SaintDK <ivan.yakimishin.00@mail.ru>
2022-06-28 13:49:58 -0400

    commit

--- kyu_8/Basic_Mathematical_Operations/BasicOperations.java
+++ kyu_8/Basic_Mathematical_Operations/BasicOperations.java
@@ -3,20 +3,19 @@ package kyu_8.Basic_Mathematical_Operations;
 public class BasicOperations
 {
     public static Integer basicMath(String op, int v1, int v2){
-        int answer = 0;
         
         if (op == "+") {
-            answer = v1 + v2;
+            return v1 + v2;
         }
         if (op == "-") {
-            answer = v1 - v2;
+            return v1 - v2;
         }
         if (op == "*") {
-            answer = v1 * v2;
+            return v1 * v2;
         }
         if (op == "/") {
-            answer = v1 / v2;
+            return v1 / v2;
         }
-        return answer;
+        return null;
     }
 }
