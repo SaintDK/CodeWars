5ac005536f795bbc3f22f3e2ef1e77f320ac0dfa
SaintDK <ivan.yakimishin.00@mail.ru>
2022-07-18 10:20:31 -0400

    commit

---
+++ kyu_8/Volume_of_a_Cuboid/Kata.java
@@ -0,0 +1,9 @@
+package kyu_8.Volume_of_a_Cuboid;
+
+public class Kata {
+
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return width * height * length;
+    }
+
+}
