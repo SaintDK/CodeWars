491f1ae50485a5753c4b6fa5f62c5cc28ccbf5e6
SaintDK <ivan.yakimishin.00@mail.ru>
2022-07-19 09:53:15 -0400

    commit

---
+++ kuy_6/DuplicateEncoderTest.java
@@ -0,0 +1,15 @@
+package kuy_6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class DuplicateEncoderTest {
+    @Test
+    public void test() {
+        assertEquals(")()())()(()()(",
+                DuplicateEncoder.encode("Prespecialized"));
+        assertEquals("))))())))",DuplicateEncoder.encode("   ()(   "));
+    }
+}
