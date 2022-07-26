7df3eb7ef616ac78711db35ca4de819b6fe5b861
SaintDK <ivan.yakimishin.00@mail.ru>
2022-07-26 06:07:21 -0400

    commit

---
+++ kyu_8/Keep_up_the_hoop/HelpAlex.java
@@ -0,0 +1,9 @@
+package kyu_8.Keep_up_the_hoop;
+
+public class HelpAlex{
+    public static String hoopCount(int n){
+        if (n >= 10) {return "Great, now move on to tricks";}
+        return "Keep at it until you get it";
+
+    }
+}
