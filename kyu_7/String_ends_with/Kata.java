23858ed7bcc0b96aa4c5e880c61fad9d556e25d7
SaintDK <ivan.yakimishin.00@mail.ru>
2022-07-29 08:49:14 -0400

    commit

--- kyu_7/String_ends_with/Kata.java
+++ kyu_7/String_ends_with/Kata.java
@@ -20,3 +20,9 @@ public class Kata {
         }
     }
 }
+
+//public class Kata {
+//    public static boolean solution(String str, String ending) {
+//        return str.endsWith(ending);
+//    }
+//}
