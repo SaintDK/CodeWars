a801350c7e39180d5bd6f0399d885f8cebafc20e
SaintDK <ivan.yakimishin.00@mail.ru>
2022-08-06 14:11:04 -0400

    commit

--- kyu_8/All_Star_Code_Challenge_18/CodeWars.java
+++ kyu_8/All_Star_Code_Challenge_18/CodeWars.java
@@ -2,6 +2,11 @@ package kyu_8.All_Star_Code_Challenge_18;
 
 public class CodeWars {
     public static int strCount(String str, char letter) {
-        return
+        int count = 0;
+        for (int i = 0; i<str.length(); i++) {
+            char[] strChar = str.toCharArray();
+            if (strChar[i] == letter) {count += 1;}
+        }
+        return count;
     }
 }
