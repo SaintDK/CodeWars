1139d507606b0999afa96be3b4f94736faa2a3f2
SaintDK <ivan.yakimishin.00@mail.ru>
2022-08-11 07:10:34 -0400

    commit

--- kyu_8/Counting_sheep/Counter.java
+++ kyu_8/Counting_sheep/Counter.java
@@ -3,7 +3,7 @@ public class Counter {
     public int countSheeps(Boolean[] arrayOfSheeps) {
         int count = 0;
         for(int i = 0; i < arrayOfSheeps.length; i++){
-            if(arrayOfSheeps[i] == true){
+            if(arrayOfSheeps[i]){
                 count ++;
             }
         }
