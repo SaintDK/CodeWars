fcbd28da99d0909133f260dd5ed2443c909583ad
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-08-18 09:06:47 -0400

    commit

---
+++ kyu_8/Even_or_Odd/EvenOrOdd.java
@@ -0,0 +1,8 @@
+package kyu_8.Even_or_Odd;
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if (number % 2 == 0){return "Even";}
+        return "Odd";
+    }
+}
