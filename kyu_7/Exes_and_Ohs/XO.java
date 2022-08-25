8bfafc36f800052c24ebba65f14e827960db4b54
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-08-25 12:25:27 -0400

    commit

--- kyu_7/Exes_and_Ohs/XO.java
+++ kyu_7/Exes_and_Ohs/XO.java
@@ -3,13 +3,15 @@ package kyu_7.Exes_and_Ohs;
 public class XO {
 
     public static boolean getXO (String str) {
+
+        str = str.toLowerCase();
         char[] strChar = str.toCharArray();
 
         int X = 0 , O = 0;
 
         for (int i=0; i<str.length(); i++) {
-            if (strChar[i] == 'x' || strChar[i] == 'X') {X += 1;}
-            if (strChar[i] == 'o' || strChar[i] == 'O') {O += 1;}
+            if (strChar[i] == 'x') {X += 1;}
+            if (strChar[i] == 'o') {O += 1;}
         }
         if (X == O) {return true;}
         return false;
