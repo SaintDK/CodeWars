ddc765b25a2ca73fd05f145b6075f3a9d6575b40
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-08-27 06:54:30 -0400

    commit

---
+++ kyu_8/Welcome/Welcome.java
@@ -0,0 +1,29 @@
+package kyu_8.Welcome;
+
+import java.util.HashMap;
+
+public class Welcome {
+    public static String greet(String language){
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("english", "Welcome");
+        map.put("czech", "Vitejte");
+        map.put("danish", "Velkomst");
+        map.put("dutch", "Welkom");
+        map.put("estonian", "Tere tulemast");
+        map.put("finnish", "Tervetuloa");
+        map.put("flemish", "Welgekomen");
+        map.put("french", "Bienvenue");
+        map.put("german", "Willkommen");
+        map.put("irish", "Failte");
+        map.put("italian", "Benvenuto");
+        map.put("latvian", "Gaidits");
+        map.put("lithuanian", "Laukiamas");
+        map.put("polish", "Witamy");
+        map.put("spanish", "Bienvenido");
+        map.put("swedish", "Valkommen");
+        map.put("welsh", "Croeso");
+
+        return map.getOrDefault(language, map.get("english"));
+    }
+}
