7c2198593075f6e9cb517f9225f518435bf87881
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-09-08 14:46:20 -0400

    commit

---
+++ kyu_8/Convert_to_Binary/Kata.java
@@ -0,0 +1,8 @@
+package kyu_8.Convert_to_Binary;
+
+public class Kata {
+
+    public static int toBinary(int n) {
+        return Integer.parseInt(Integer.toBinaryString(n));
+    }
+}
---
+++ kyu_8/Convert_to_Binary/KataTest.java
@@ -0,0 +1,16 @@
+package kyu_8.Convert_to_Binary;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class KataTest {
+    @Test
+    public void testToBinary() {
+        assertEquals(1, Kata.toBinary(1));
+        assertEquals(10, Kata.toBinary(2));
+        assertEquals(11, Kata.toBinary(3));
+        assertEquals(101, Kata.toBinary(5));
+    }
+}
