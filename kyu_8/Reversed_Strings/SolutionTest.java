b52c7a78ca6bcd3d9b6fab885fafea08f0af6e6b
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-09-12 10:50:59 -0400

    commit

---
+++ kyu_8/Reversed_Strings/SolutionTest.java
@@ -0,0 +1,12 @@
+package kyu_8.Reversed_Strings;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void sampleTests() {
+        assertEquals("dlrow", Kata.solution("world"));
+    }
+}
