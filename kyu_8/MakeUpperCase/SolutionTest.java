6bab5696bbc2c9d95779d60de1e744672324f035
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-09-27 12:08:15 -0400

    commit

---
+++ kyu_8/MakeUpperCase/SolutionTest.java
@@ -0,0 +1,15 @@
+package kyu_8.MakeUpperCase;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+
+    Upper u = new Upper();
+
+    @Test
+    public void testSomething() {
+        assertEquals("HELLO",u.MakeUpperCase("hello"));
+    }
+}
---
+++ kyu_8/MakeUpperCase/Upper.java
@@ -0,0 +1,7 @@
+package kyu_8.MakeUpperCase;
+
+class Upper{
+    public static String MakeUpperCase(String str){
+        return str.toUpperCase();
+    }
+}
