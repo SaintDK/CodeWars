d5127f5bb495b6474acfee7cbb514977406f297e
SaintDK <107093295+SaintDK@users.noreply.github.com>
2022-11-08 12:06:20 -0500

    commit

--- kyu_7/Odd_or_Even/Codewars.java
+++ kyu_7/Odd_or_Even/Codewars.java
@@ -1,12 +1,9 @@
 package kyu_7.Odd_or_Even;
 
-public class Codewars {
-    public static String oddOrEven (int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) {
-                return "odd";
-            }
-        }
-        return "0";
+import static java.util.Arrays.stream;
+
+class Codewars {
+    static String oddOrEven(final int[] array) {
+        return stream(array).sum() % 2 == 0 ? "even" : "odd";
     }
 }
