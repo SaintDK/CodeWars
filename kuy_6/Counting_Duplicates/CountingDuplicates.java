93fbc42253d076dde5c7aca24cc5448d68f8e96f
ivany <ivan.yakimishin.00@mail.ru>
2022-11-15 20:59:25 +0300

    Commit

---
+++ kuy_6/Counting_Duplicates/CountingDuplicates.java
@@ -0,0 +1,15 @@
+package kuy_6.Counting_Duplicates;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        int ans = 0;
+        text = text.toLowerCase();
+        while (text.length() > 0) {
+            String firstLetter = text.substring(0,1);
+            text = text.substring(1);
+            if (text.contains(firstLetter)) ans ++;
+            text = text.replace(firstLetter, "");
+        }
+        return ans;
+    }
+}
