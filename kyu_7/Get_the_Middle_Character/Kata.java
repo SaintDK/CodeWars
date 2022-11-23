97be421b3621fb04e82cff5acf0e2824637e73e2
ivany <ivan.yakimishin.00@mail.ru>
2022-11-23 10:47:28 +0300

    Commit

---
+++ kyu_7/Get_the_Middle_Character/Kata.java
@@ -0,0 +1,8 @@
+package kyu_7.Get_the_Middle_Character;
+
+class Kata {
+    public static String getMiddle(String word) {
+        int length = word.length();
+        return (length % 2 != 0) ?  String.valueOf(word.charAt(length / 2)) : word.substring(length / 2 - 1, length / 2 + 1);
+    }
+}
