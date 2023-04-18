4896226514e04e36cb20ab46b17c6c5d63af3cb4
ivany <ivan.yakimishin.00@mail.ru>
2023-04-18 14:47:29 +0300

    Commit

---
+++ kyu_7/Binary_Addition/Kata.java
@@ -0,0 +1,11 @@
+package kyu_7.Binary_Addition;
+
+public class Kata{
+
+    public static String binaryAddition(int a, int b){
+
+        String answer = Integer.toBinaryString(a + b);
+
+        return answer;
+    }
+}
