016aaeafea004c8092cb4266d1d214fd29b9fa34
ivany <ivan.yakimishin.00@mail.ru>
2023-04-22 12:06:34 +0300

    Commit

---
+++ kyu_7/Filter_the_number/Kata.java
@@ -0,0 +1,10 @@
+package kyu_7.Filter_the_number;
+
+public class Kata {
+    public static long filterString(final String value) {
+
+        long num = Long.parseLong(value);
+
+        return num; // complete this function :)
+    }
+}
