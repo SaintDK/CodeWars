9a9bf908e40d09e9f484ef1bfbb6b38ee55eb8f8
ivany <ivan.yakimishin.00@mail.ru>
2023-05-10 20:45:01 +0300

    Commit

--- kyu_7/Bumps_in_the_Road/BumpsTheRoad.java
+++ kyu_7/Bumps_in_the_Road/BumpsTheRoad.java
@@ -3,7 +3,7 @@ package kyu_7.Bumps_in_the_Road;
 public class BumpsTheRoad {
     public static String bumps(String road) {
 
-        road = road.replace("_", "");
+        road = road.replaceAll("[_]", "");
         if (road.length() > 15) {return "Car Dead";}
 
         return "Woohoo!";
