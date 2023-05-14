b2a34e9ef689dda98beb7718aa29f9905ea2671d
ivany <ivan.yakimishin.00@mail.ru>
2023-05-14 19:33:03 +0300

    Commit

--- kyu_8/Find_the_position/Kata.java
+++ kyu_8/Find_the_position/Kata.java
@@ -1,38 +1,44 @@
 package kyu_8.Find_the_position;
 
+//abcdefghijklmnopqrstuvwxyz
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Kata
 {
     public static String position(char alphabet)
     {
-        switch (alphabet) {
-            case 'a': return "Position of alphabet: 1";
-            case 'b': return "Position of alphabet: 2";
-            case 'c': return "Position of alphabet: 3";
-            case 'd': return "Position of alphabet: 4";
-            case 'e': return "Position of alphabet: 5";
-            case 'f': return "Position of alphabet: 6";
-            case 'g': return "Position of alphabet: 7";
-            case 'h': return "Position of alphabet: 8";
-            case 'i': return "Position of alphabet: 9";
-            case 'j': return "Position of alphabet: 10";
-            case 'k': return "Position of alphabet: 11";
-            case 'l': return "Position of alphabet: 12";
-            case 'm': return "Position of alphabet: 13";
-            case 'n': return "Position of alphabet: 14";
-            case 'o': return "Position of alphabet: 15";
-            case 'p': return "Position of alphabet: 16";
-            case 'q': return "Position of alphabet: 17";
-            case 'r': return "Position of alphabet: 18";
-            case 's': return "Position of alphabet: 19";
-            case 't': return "Position of alphabet: 20";
-            case 'u': return "Position of alphabet: 21";
-            case 'v': return "Position of alphabet: 22";
-            case 'w': return "Position of alphabet: 23";
-            case 'x': return "Position of alphabet: 24";
-            case 'y': return "Position of alphabet: 25";
-            case 'z': return "Position of alphabet: 26";
-        }
-        return null;
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+
+        map.put('a', 1);
+        map.put('b', 2);
+        map.put('c', 3);
+        map.put('d', 4);
+        map.put('e', 5);
+        map.put('f', 6);
+        map.put('g', 7);
+        map.put('h', 8);
+        map.put('i', 9);
+        map.put('j', 10);
+        map.put('k', 11);
+        map.put('l', 12);
+        map.put('m', 13);
+        map.put('n', 14);
+        map.put('o', 15);
+        map.put('p', 16);
+        map.put('q', 17);
+        map.put('r', 18);
+        map.put('s', 19);
+        map.put('t', 20);
+        map.put('u', 21);
+        map.put('v', 22);
+        map.put('w', 23);
+        map.put('x', 24);
+        map.put('y', 25);
+        map.put('z', 26);
+
+        return "Position of alphabet: " + map.get(alphabet);
+
     }
 }
-//abcdefghijklmnopqrstuvwxyz
