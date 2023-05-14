d6c8ab50d45d4eb4d3f186ab77190931f6b00936
ivany <ivan.yakimishin.00@mail.ru>
2023-05-14 18:30:44 +0300

    Commit

---
+++ kyu_8/Find_the_position/SolutionTest.java
@@ -0,0 +1,14 @@
+package kyu_8.Find_the_position;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class SolutionTest {
+    @Test
+    public void basicTests() {
+        assertEquals("Position of alphabet: 1", Kata.position('a'));
+        assertEquals("Position of alphabet: 26", Kata.position('z'));
+        assertEquals("Position of alphabet: 5", Kata.position('e'));
+    }
+}
