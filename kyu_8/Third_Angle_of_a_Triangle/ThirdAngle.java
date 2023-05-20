28b0eaa27bc62a277c4b43e31012b4e2d6e23bc4
ivany <ivan.yakimishin.00@mail.ru>
2023-05-20 14:39:11 +0300

    Commit

--- kyu_8/Third_Angle_of_a_Triangle/ThirdAngle.java
+++ kyu_8/Third_Angle_of_a_Triangle/ThirdAngle.java
@@ -2,8 +2,5 @@ package kyu_8.Third_Angle_of_a_Triangle;
 
 
 public class ThirdAngle {
-    public static int otherAngle(int angle1, int angle2) {
-        // TODO: Place code here
-        return 180 - angle1 - angle2;
-    }
+    public static int otherAngle(int angle1, int angle2) {return 180 - angle1 - angle2;}
 }
