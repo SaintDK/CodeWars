8da8fadc2115fbc4cd31986d317bfd16b91ddbd0
ivany <ivan.yakimishin.00@mail.ru>
2023-06-07 17:04:02 +0300

    Commit

--- kyu_8/Reversed_sequence/Sequence.java
+++ kyu_8/Reversed_sequence/Sequence.java
@@ -1,17 +1,12 @@
 package kyu_8.Reversed_sequence;
 
+import java.util.stream.IntStream;
+
 public class Sequence{
 
     public static int[] reverse(int n){
-        if (n<=0) return null;
 
-        int[] out = new int[n];
-
-        for (int x=n, i=0; x>0; x--) {
-            out[i] = x;
-            i++;
-        }
-        return out;
+        return IntStream.range(-n, 0).map(Math::abs).toArray();
     }
 
 }
