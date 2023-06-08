722b45b872c656bbf9e268cc7f2ca5f13494320d
ivany <ivan.yakimishin.00@mail.ru>
2023-06-08 19:47:57 +0300

    Commit

--- kyu_8/Beginner_Series_1_School_Paperwork/Paper.java
+++ kyu_8/Beginner_Series_1_School_Paperwork/Paper.java
@@ -2,8 +2,8 @@ package kyu_8.Beginner_Series_1_School_Paperwork;
 
 public class Paper
 {
-    public static int paperWork(int n, int m) {
-        if (n < 0 || m < 0) {return 0;}
-        return n * m;
+    public static int paperWork(int n, int m)
+    {
+        return (n<0 | m<0) ? 0: (m*n);
     }
 }
