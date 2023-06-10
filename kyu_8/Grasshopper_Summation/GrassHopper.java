f1fd3f9ac5fd37100512355a60cffb86842d20aa
ivany <ivan.yakimishin.00@mail.ru>
2023-06-10 23:38:50 +0300

    Commit

--- kyu_8/Grasshopper_Summation/GrassHopper.java
+++ kyu_8/Grasshopper_Summation/GrassHopper.java
@@ -2,9 +2,8 @@ package kyu_8.Grasshopper_Summation;
 
 public class GrassHopper {
 
-    public static Object summation(int n) {
-        int answer = 0, count = 0;
-        for (int i = 0; i<n;i++) {answer += 1+count;count++;}
-        return answer;
+    public static int summation(int n) {
+
+        return  n*(n+1)/2;
     }
 }
