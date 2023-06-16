42037b215ab0e99435afbbda4b9832377c8a8f3a
SaintDK <107093295+SaintDK@users.noreply.github.com>
2023-06-16 22:44:18 +0300

    Update XO.java

--- kyu_7/Exes_and_Ohs/XO.java
+++ kyu_7/Exes_and_Ohs/XO.java
@@ -1,19 +1,10 @@
 package kyu_7.Exes_and_Ohs;
 
 public class XO {
-
-    public static boolean getXO (String str) {
-
-        str = str.toLowerCase();
-        char[] strChar = str.toCharArray();
-
-        int X = 0 , O = 0;
-
-        for (int i=0; i<str.length(); i++) {
-            if (strChar[i] == 'x') {X += 1;}
-            if (strChar[i] == 'o') {O += 1;}
-        }
-        if (X == O) {return true;}
-        return false;
-    }
+  
+  public static boolean getXO (String str) {
+    str = str.toLowerCase();
+    return str.replace("o","").length() == str.replace("x","").length();
+    
+  }
 }
