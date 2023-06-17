79587b3725e256a7ee215a47819bfdacf3b962de
SaintDK <107093295+SaintDK@users.noreply.github.com>
2023-06-17 21:46:40 +0300

    Update Solution.java

--- kuy_6/Break_camelCase/Solution.java
+++ kuy_6/Break_camelCase/Solution.java
@@ -7,3 +7,12 @@ class Solution {
         return input.replaceAll("([A-Z])", " $1");
     }
 }
+//class Solution {
+//  public static String camelCase(String input) {
+//  String output = "";
+//        for (int i = 0; i < input.length(); i++) {
+//            output = Character.isUpperCase(input.charAt(i)) ? output + " " + input.charAt(i) : output + input.charAt(i);
+//       }
+//        return output;
+//  }
+//}
