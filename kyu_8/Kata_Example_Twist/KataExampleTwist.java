fa40da92aa631f1231159586eb54148ceed406f6
ivany <ivan.yakimishin.00@mail.ru>
2023-06-23 22:10:59 -1200

    commit

---
+++ kyu_8/Kata_Example_Twist/KataExampleTwist.java
@@ -0,0 +1,14 @@
+package kyu_8.Kata_Example_Twist;
+
+
+import java.util.Arrays;
+
+public class KataExampleTwist
+{
+    public static String[] kataExampleTwist()
+    {
+        String[] websites = new String[1000];
+        Arrays.fill(websites,"codewars");
+        return websites;
+    }
+}
